744b4b03be465b8ad067c28ae0b14c8b1dbacb06
Joaquim Ley <me@joaquimley.com>
2018-04-09 18:11:15 +0200

    Dependency housekeeping

---
+++ transport-eta-android/mobile-ui/src/androidTest/java/com/joaquimley/transporteta/SmsControllerTest.kt
@@ -0,0 +1,27 @@
+package com.joaquimley.transporteta
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import com.joaquimley.transporteta.sms.SmsBroadcastReceiver
+import com.joaquimley.transporteta.sms.SmsController
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class SmsControllerTest {
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.joaquimley.transporteta", appContext.packageName)
+    }
+}
