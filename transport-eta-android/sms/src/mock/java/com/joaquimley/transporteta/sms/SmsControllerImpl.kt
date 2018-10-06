40ce079a0081efe9bea76e243cb7f69a9726ad8b
Joaquim Ley <me@joaquimley.com>
2018-10-07 00:36:49 +0200

    Scaffold :sms unit tests for CI integration, dependencies update
    - Also made some changes to the README copy.

--- transport-eta-android/sms/src/mock/java/com/joaquimley/transporteta/sms/SmsControllerImpl.kt
+++ transport-eta-android/sms/src/mock/java/com/joaquimley/transporteta/sms/SmsControllerImpl.kt
@@ -4,6 +4,7 @@ import android.telephony.SmsManager
 import android.util.Log
 import com.joaquimley.transporteta.sms.model.SmsModel
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -13,34 +14,16 @@ import javax.inject.Singleton
 
 @Singleton
 open class SmsControllerImpl @Inject constructor(private val smsBroadcastReceiver: SmsBroadcastReceiver) : SmsController {
-
-    private val serviceSms: PublishSubject<SmsModel> = PublishSubject.create()
-    private var busStopCode: Int = 0
-
-    private val disposable: Disposable?
-
-    init {
-        // TODO: There might be an issue with race condition (busStopCode)
-        // TODO create a queue of requests and handle each synchronously
-        disposable = smsBroadcastReceiver.observeServiceSms()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ serviceSms.onNext(SmsModel(busStopCode, it)) },
-                        { Log.e("SmsController", "Failed smsBroadcastReceiver.observeServiceSms(): ${it.message}")})
-    }
-
-    override fun observeIncomingSms(): Observable<SmsModel> {
-        return serviceSms
-    }
-
-    override fun requestEta(busStopCode: Int) {
-        this.busStopCode = busStopCode
-        SmsManager.getDefault().sendTextMessage(smsBroadcastReceiver.serviceNumber, null, "C $busStopCode", null, null)
-        Log.e("SmsController", "requestEta $busStopCode")
-    }
-
     override fun dispose() {
-        disposable?.dispose()
+
+    }
+
+    override fun invalidateRequest() {
+
+    }
+
+    override fun requestEta(busStopCode: Int): Single<SmsModel> {
+        return Single.just(SmsModel(busStopCode, "MockData"))
     }
 }
 
---
+++ transport-eta-android/sms/src/test/java/com/joaquimley/SmsControllerTest.kt
@@ -0,0 +1,49 @@
+package com.joaquimley
+
+import com.joaquimley.transporteta.sms.SmsBroadcastReceiver
+import com.joaquimley.transporteta.sms.SmsController
+import com.joaquimley.transporteta.sms.SmsControllerImpl
+import com.nhaarman.mockitokotlin2.atLeastOnce
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.Observable
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class SmsControllerTest {
+
+    private val robot = Robot()
+    private val mockSmsBroadcastReceiver = mock<SmsBroadcastReceiver>()
+
+    lateinit var smsController: SmsController
+
+    @Before
+    fun setup() {
+        robot.stubSmsBroadcastReceiverSuccess()
+        smsController = SmsControllerImpl(mockSmsBroadcastReceiver)
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun onCreationListensToSmsBroadcastReceiver() {
+        // Assemble
+        robot.stubSmsBroadcastReceiverSuccess()
+        // Act
+        // No action
+        // Assert
+        verify(mockSmsBroadcastReceiver, atLeastOnce()).observeServiceSms()
+    }
+
+    inner class Robot {
+        fun stubSmsBroadcastReceiverSuccess(message: String = "randomString"): String {
+            whenever(mockSmsBroadcastReceiver.observeServiceSms()).then { Observable.just(message) }
+            return message
+        }
+    }
+}
